571238255d1c0c33b1f9293e1d69834434645415
Dominik Skerhak <dominik.skerhak@gmail.com>
2019-10-09 10:15:01 +0200

    Create new GiftsModule
    - Move old gift coupons from ProductsModule to new GiftsModule.
    - Create GiftsModule init migration, translations, composer, etc.
    - Initialize GiftsModule in main config.
    - Add gift subscriptions purchaseable from sales funnels.
    
    Required fields which needs to be passed from sales funnel within
    `payment_metadata`:
    
    - `gift` (boolean) - if true, indicates that this purchase is gift.
    - `gift_email` (string) - email of customer for which is gift
      purchased. This can be email of existing or new customer. Gift
      process handles creation of new accounts.
    - String `gift_starts_at` - date when should gifted subscription
      start. Expects RFC 3339 datetime format.
    
    ---
    
    Processing of gift subscription purchased via SalesFunnel is different
    than when purchased within Shop. Main difference is that payment
    created in shop (ProductsModule) is processed within ProductsModule.
    Purchase done within SalesFunnel is handled by PaymentsModule. We are
    now just hooking on both processes via events & handlers.
    
    Before payment is created, `PaymentItemContainerReadyEventHandler`:
    
    - If payment metadata contains required fields (described above),
      PaymentItem is switched from previous `SubscriptionType PaymentItem`
      to new `GiftPaymentItem`.
    
    After payment is created, `CreateGiftCouponNewPaymentEventHandler`:
    
    - Creates `payment_gift_coupon` entry for new gift (keeping it
      similiar to gift coupons purchased via shop; will be probably later
      refactored). This is safe because gift coupon is processed (and
      sent) after payment is paid.
    - Removes `subscription_type_id` from payment (which stops creation
      of subscription after payment is paid). We don't want to create
      subscription for giver of gift subscription.
    
    After payment is paid:
    
    - Giver is asked for Gift address which is moved to donee after user
      is created. Added form & widget for this new gift address.
      - Not asking for traditional print address which is linked to giver and
        could be already prefilled with his current print address. This is now
        enabled only for subscription_type payment items.
      - Address for gift subscription is linked to giver and after
        account & subscription for donee are created, address is moved under
        donee account as print type address.
    - Listener `GiftPaymentStatusChangeHandler` emits NotificationEvent
      `created_payment_gift_coupon` with coupon attachment (this is set in
      admin configuration).
    
    Command `ActivatePurchasedGiftCouponsCommand` running in cron, checks
    for each coupon if payment was paid (legacy, see notes below). If all
    conditions are met:
    
    - Command `ActivatePurchasedGiftCouponsCommand` activates gift
      subscription.
    
    Notes:
    
    - Added `PaymentItemContainer::switchItem()` to switch `$oldItem item
      to `newItem`. Exception is thrown if `$oldItem` is not present with
      `$itemIndex` in PaymentItemContainer.
    - PaymentGiftCoupon entry is created before payment if confirmed & PAID.
      This is legacy issue. ProductsModule is storing coupon information
      (email & start date) here because there were no `payment_meta` data in
      past.
    - New widget `GiftPaymentItemsListWidget` for gift type PaymentItem
      registered to `payments.admin.payment_item_listing` which is shown
      within user's and payment's administration.
    
    remp/novydenik#460

--- src/components/PaymentSuccessPrintWidget/PaymentSuccessPrintWidget.php
+++ src/components/PaymentSuccessPrintWidget/PaymentSuccessPrintWidget.php
@@ -8,6 +8,7 @@ use Crm\PaymentsModule\Repository\PaymentLogsRepository;
 use Crm\PaymentsModule\Repository\PaymentsRepository;
 use Crm\PrintModule\Forms\UserPrintAddressFormFactory;
 use Crm\SalesFunnelModule\Presenters\SalesFunnelPresenter;
+use Crm\SubscriptionsModule\PaymentItem\SubscriptionTypePaymentItem;
 
 /**
  * PaymentSuccessPrintWidget is directly targeted to be used in \Crm\SalesFunnelModule\Presenters\SalesFunnelPresenter
@@ -79,6 +80,11 @@ class PaymentSuccessPrintWidget extends BaseWidget
     private function isPrintAddressRequired($payment)
     {
         foreach ($payment->related('payment_items') as $paymentItem) {
+            // ask address only for subscription type payment items
+            if ($paymentItem->type !== SubscriptionTypePaymentItem::TYPE) {
+                continue;
+            }
+
             $subscriptionType = $paymentItem->subscription_type;
             if (!$subscriptionType) {
                 continue;
